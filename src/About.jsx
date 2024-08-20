a7cba6685ce0baaa871ad0666b6b84a701e2edbc
NMasters52 <168689746+NMasters52@users.noreply.github.com>
2024-08-20 12:50:32 -0400

    refractor about__text pt2

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
 
     return (
         <div className='about'>
-            <p className='about__text'>I’m a passionate front-end web developer with a solid foundation in HTML, CSS, JavaScript, React, and PHP. Driven by creativity and a love for problem-solving, I’m committed to crafting seamless, user-friendly interfaces. My recent training at Jax Code has equipped me with the skills to bring innovative ideas to life on the web. As someone who is adaptable, quick to learn, and always eager to grow, I’m excited to contribute to a dynamic team and help build exceptional digital experiences.</p>
+            <p className='about__text'>I’m a passionate front-end web developer with a solid foundation in HTML, CSS, JavaScript, React, and PHP. Driven by creativity and a love for problem-solving. As someone who is adaptable, quick to learn, and always eager to grow, I’m excited to contribute to a dynamic team and help build exceptional digital experiences.</p>
             <div className="contact-info">
                 <div className="contact-item">
                     <i className="fa-solid fa-phone contact__icon"></i>
